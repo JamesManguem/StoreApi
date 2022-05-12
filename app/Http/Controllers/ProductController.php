57532162b726bb4e8b7363e6163ef125a52e653b
JamesManguem <18090067@alumno.utmetropolitana.edu.mx>
2022-05-12 09:41:20 -0400

    Adjuntar archivos

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,6 +3,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Product;
+use Carbon\Carbon;
 
 class ProductController extends Controller{
 
@@ -19,15 +20,41 @@ class ProductController extends Controller{
       
 
           $dataProduct = new Product; 
-          
+
+          if($request->hasFile('Picture')) { 
+
+            $nombreArchivoOriginal=$request->file('Picture')->getClientOriginalName();
+
+            $nuevoNombre= Carbon::now()->timestamp."_".$nombreArchivoOriginal;
+
+            $carpetaDestino='./upload/';
+
+            $request->file('Picture')->move($carpetaDestino, $nuevoNombre);
+
+            
           $dataProduct -> Name = $request->Name;
+
           $dataProduct -> Description = $request->Description;
+
           $dataProduct -> Price = $request->Price;
+
           $dataProduct -> Stock = $request->Stock;
-          $dataProduct -> Picture = $request->Picture;
+
+          $dataProduct -> Picture = ltrim($carpetaDestino,'.').$nuevoNombre;
 
           $dataProduct ->save();
 
+
+
+
+
+
+
+
+          }
+
+
+          
 
         return response()->json($request);
 
